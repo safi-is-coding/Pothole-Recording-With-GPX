f744739ee5b7d2df1ad49d01857f6ae36237fc9b
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-05-28 00:10:48 +0530

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './'
+//   // server: {
+//   //   allowedHosts: ["2794-2401-4900-8836-87fb-8584-6efc-bf22-44f6.ngrok-free.app"], 
+//   // },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
-  // server: {
-  //   allowedHosts: ["2794-2401-4900-8836-87fb-8584-6efc-bf22-44f6.ngrok-free.app"], 
-  // },
+  base: './', // Important for relative paths in production
 });
