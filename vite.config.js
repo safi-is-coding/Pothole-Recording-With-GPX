a7714d6440b300e6c4d77cdc30dd403dad4a8901
safi-is-coding <cse.mohammadsafi@gmail.com>
2025-05-28 00:04:07 +0530

    `Added base option to Vite config`

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './'
   // server: {
   //   allowedHosts: ["2794-2401-4900-8836-87fb-8584-6efc-bf22-44f6.ngrok-free.app"], 
   // },
